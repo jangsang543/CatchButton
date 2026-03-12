474f133b3c3d0dd97ba35cfb402b6b91bb4f2154
장상 <jangsang527@gmail.com>
2026-03-12 16:29:12 +0900

    버튼을 잡아라 게임

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,18 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            myButton = new Button();
             SuspendLayout();
+            // 
+            // myButton
+            // 
+            myButton.BackColor = Color.FromArgb(255, 255, 128);
+            myButton.Font = new Font("휴먼둥근헤드라인", 36F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            myButton.ForeColor = Color.Fuchsia;
+            myButton.Location = new Point(347, 282);
+            myButton.Name = "myButton";
+            myButton.Size = new Size(493, 156);
+            myButton.TabIndex = 0;
+            myButton.Text = "나를 잡아봐";
+            myButton.UseVisualStyleBackColor = false;
+            myButton.MouseEnter += myButton_MouseEnter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(992, 614);
+            ClientSize = new Size(1250, 790);
+            Controls.Add(myButton);
             Name = "Form1";
-            Text = "Form1";
+            Text = "버튼 잡기 게임";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Button myButton;
     }
 }
